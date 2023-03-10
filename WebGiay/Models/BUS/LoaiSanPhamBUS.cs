575a68f9167cf81591c97e7dafb57aa4b9c8deff
LeeHien99 <hienlemanh2002@gmail.com>
2023-03-10 22:30:00 +0700

    Update Men And Women Partial View

---
+++ WebGiay/Models/BUS/LoaiSanPhamBUS.cs
@@ -0,0 +1,23 @@
+﻿using ShopOnlineConnection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebGiay.Models.BUS
+{
+    public class LoaiSanPhamBUS
+    {
+        public static IEnumerable<LoaiSanPham> DanhSach()
+        {
+            var db = new ShopOnlineConnectionDB();
+            return db.Query<LoaiSanPham>("Select * from LoaiSanPham where TinhTrang = 0");
+        }
+
+        public static IEnumerable<SanPham> ChiTiet(String id)
+        {
+            var db = new ShopOnlineConnectionDB();
+            return db.Query<SanPham>("Select * from SanPham where MaLoaiSanPham = "+id+"");
+        }
+    }
+}
---
+++ WebGiay/Models/BUS/NhaSanXuatBUS.cs
@@ -0,0 +1,23 @@
+﻿using ShopOnlineConnection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebGiay.Models.BUS
+{
+    public class NhaSanXuatBUS
+    {
+        public static IEnumerable<NhaSanXuat> DanhSach()
+        {
+            var db = new ShopOnlineConnectionDB();
+            return db.Query<NhaSanXuat>("Select * from NhaSanXuat where TinhTrang = 0");
+        }
+
+        public static IEnumerable<SanPham> ChiTiet(String id)
+        {
+            var db = new ShopOnlineConnectionDB();
+            return db.Query<SanPham>("Select * from SanPham where MaNhaSanXuat = '" + id +"'");
+        }
+    }
+}
